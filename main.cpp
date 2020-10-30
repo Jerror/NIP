faccd53f2fa4909c2f1876ffefc22f41e8d0fddd
Jeremiah O'Neil <jeremiah.d.oneil@gmail.com>
2020-10-30 17:36:24 -0400

    trivial change to tests

--- main.cpp
+++ main.cpp
@@ -11,22 +11,22 @@ template <typename T>
 T cubic_pp(T x) { return 6 * x - 2; }
 
 template <typename T>
-void test_NIP(int maxiter, T tol)
+void test_NIP(T x0, int maxiter, T tol)
 {
     NewtonIterativeProcedure<T>* NIP;
     RootResults<T> res;
 
-    NIP = new NewtonIterativeProcedure<T>(cubic<T>, 0.5);
+    NIP = new NewtonIterativeProcedure<T>(cubic<T>, x0);
     res = NIP->solve(maxiter, tol);
     std::cout << res.root << " " << res.iterations << " " << res.function_calls
               << " " << res.flag << std::endl;
 
-    NIP = new NewtonIterativeProcedure<T>(cubic<T>, cubic_p<T>, 0.5);
+    NIP = new NewtonIterativeProcedure<T>(cubic<T>, cubic_p<T>, x0);
     res = NIP->solve(maxiter, tol);
     std::cout << res.root << " " << res.iterations << " " << res.function_calls
               << " " << res.flag << std::endl;
 
-    NIP = new NewtonIterativeProcedure<T>(cubic<T>, cubic_p<T>, cubic_pp<T>, 0.5);
+    NIP = new NewtonIterativeProcedure<T>(cubic<T>, cubic_p<T>, cubic_pp<T>, x0);
     res = NIP->solve(maxiter, tol);
     std::cout << res.root << " " << res.iterations << " " << res.function_calls
               << " " << res.flag << std::endl;
@@ -35,9 +35,9 @@ void test_NIP(int maxiter, T tol)
 int main(int argc, char* argv[])
 {
     std::cout << "float" << std::endl;
-    test_NIP<float>(20, 1e-8);
+    test_NIP<float>(1.5, 20, 1e-8);
     std::cout << "double" << std::endl;
-    test_NIP<double>(40, 1e-16);
+    test_NIP<double>(1.5, 40, 1e-16);
     std::cout << "long double" << std::endl;
-    test_NIP<long double>(80, 1e-32);
+    test_NIP<long double>(1.5, 80, 1e-32);
 }
